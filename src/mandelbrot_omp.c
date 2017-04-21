02d61e0088d11a98c9fb35f8aa01d2c20c2c896a
Mateus Rocha <mateus.mazzari@usp.br>
2017-04-21 15:48:58 -0300

    backup com alocacao e saida funcionando sem alocacao e saida

--- src/mandelbrot_omp.c
+++ src/mandelbrot_omp.c
@@ -14,44 +14,13 @@ double pixel_height;
 int iteration_max = 200;
 
 int image_size;
-unsigned char **image_buffer;
 
 int i_x_max;
 int i_y_max;
 int image_buffer_size;
 
-int gradient_size = 16;
-int colors[17][3] = {
-                        {66, 30, 15},
-                        {25, 7, 26},
-                        {9, 1, 47},
-                        {4, 4, 73},
-                        {0, 7, 100},
-                        {12, 44, 138},
-                        {24, 82, 177},
-                        {57, 125, 209},
-                        {134, 181, 229},
-                        {211, 236, 248},
-                        {241, 233, 191},
-                        {248, 201, 95},
-                        {255, 170, 0},
-                        {204, 128, 0},
-                        {153, 87, 0},
-                        {106, 52, 3},
-                        {16, 16, 16},
-                    };
-
-void allocate_image_buffer(){
-    int rgb_size = 3;
-    image_buffer = (unsigned char **) malloc(sizeof(unsigned char *) * image_buffer_size);
-
-    for(int i = 0; i < image_buffer_size; i++){
-        image_buffer[i] = (unsigned char *) malloc(sizeof(unsigned char) * rgb_size);
-    };
-};
-
 void init(int argc, char *argv[]){
-    printf("%d\n", omp_get_max_threads());
+    // printf("%d\n", omp_get_max_threads());
     if(argc < 6){
         printf("usage: ./mandelbrot_omp c_x_min c_x_max c_y_min c_y_max image_size\n");
         printf("examples with image_size = 11500:\n");
@@ -70,48 +39,12 @@ void init(int argc, char *argv[]){
 
         i_x_max           = image_size;
         i_y_max           = image_size;
-        image_buffer_size = image_size * image_size;
 
         pixel_width       = (c_x_max - c_x_min) / i_x_max;
         pixel_height      = (c_y_max - c_y_min) / i_y_max;
     };
 };
 
-void update_rgb_buffer(int iteration, int x, int y){
-    int color;
-
-    if(iteration == iteration_max){
-        image_buffer[(i_y_max * y) + x][0] = colors[gradient_size][0];
-        image_buffer[(i_y_max * y) + x][1] = colors[gradient_size][1];
-        image_buffer[(i_y_max * y) + x][2] = colors[gradient_size][2];
-    }
-    else{
-        color = iteration % gradient_size;
-
-        image_buffer[(i_y_max * y) + x][0] = colors[color][0];
-        image_buffer[(i_y_max * y) + x][1] = colors[color][1];
-        image_buffer[(i_y_max * y) + x][2] = colors[color][2];
-    };
-};
-
-void write_to_file(){
-    FILE * file;
-    char * filename               = "output_omp.ppm";
-    char * comment                = "# ";
-
-    int max_color_component_value = 255;
-
-    file = fopen(filename,"wb");
-
-    fprintf(file, "P6\n %s\n %d\n %d\n %d\n", comment,
-            i_x_max, i_y_max, max_color_component_value);
-
-    for(int i = 0; i < image_buffer_size; i++){
-        fwrite(image_buffer[i], 1 , 3, file);
-    };
-
-    fclose(file);
-};
 
 void compute_mandelbrot(){
     double z_x;
@@ -159,51 +92,11 @@ void compute_mandelbrot(){
             z_x_squared = z_x * z_x;
             z_y_squared = z_y * z_y;
         };
-
-        update_rgb_buffer(iteration, i_x, i_y);
     }
-
-    // for(i_y = 0; i_y < i_y_max; i_y++){
-    //     c_y = c_y_min + i_y * pixel_height;
-    //
-    //     if(fabs(c_y) < pixel_height / 2){
-    //         c_y = 0.0;
-    //     };
-    //
-    //     //pragma
-    //     for(i_x = 0; i_x < i_x_max; i_x++){
-    //         c_x         = c_x_min + i_x * pixel_width;
-    //
-    //         z_x         = 0.0;
-    //         z_y         = 0.0;
-    //
-    //         z_x_squared = 0.0;
-    //         z_y_squared = 0.0;
-    //
-    //         for(iteration = 0;
-    //             iteration < iteration_max && \
-    //             ((z_x_squared + z_y_squared) < escape_radius_squared);
-    //             iteration++){
-    //             z_y         = 2 * z_x * z_y + c_y;
-    //             z_x         = z_x_squared - z_y_squared + c_x;
-    //
-    //             z_x_squared = z_x * z_x;
-    //             z_y_squared = z_y * z_y;
-    //         };
-    //
-    //         update_rgb_buffer(iteration, i_x, i_y);
-    //     };
-    // };
 };
 
 int main(int argc, char *argv[]){
     init(argc, argv);
-
-    allocate_image_buffer();
-
     compute_mandelbrot();
-
-    write_to_file();
-
     return 0;
 };
