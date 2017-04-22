fe678965ede2f8c72fb5fac85ecb7e90484d6982
Felipe Brigalante <felipe.brigalante@gmail.com>
2017-04-22 14:15:34 -0300

    chunk 8

--- src/mandelbrot_omp_io.c
+++ src/mandelbrot_omp_io.c
@@ -128,7 +128,7 @@ void compute_mandelbrot(){
     double c_x;
     double c_y;
 
-    int chunk = 10;
+    int chunk = 8;
 
     #pragma omp parallel for \
         private(z_x, z_y, z_x_squared, z_y_squared, iteration, i_x, i_y, i_k, c_x, c_y)\
