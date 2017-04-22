77d5288a5d3c903ec193ba30633705415f26a5cd
Mateus Rocha <mateus.mazzari@usp.br>
2017-04-22 15:12:04 -0300

    alguns plots

--- src/asd.py
+++ src/asd.py
@@ -21,35 +21,49 @@ def make_plot(arg):
     lines[line] = []
 
   for item in data:
-    if(item[fixed_x_type] == fixed_x_value and item[fixed_line_type] == fixed_line_value and item[line_type] in line_value):
+    if(item[fixed_x_type] == fixed_x_value and item[fixed_line_type] == fixed_line_value and item[line_type] in line_value and item[x_type] in x_value):
       lines[item[line_type]].append((item[x_type], item["time"], item["deviation"]))
 
   subtitle = []
+  fig = plt.figure()
+  ax = plt.subplot(111)
   for line in lines.items():
-    c = colors.pop()
-    line[1].sort(key = lambda x: x[0])
-    axis = map(list, zip(*line[1]))
-    subtitle.append(line[0])
-    plt.plot(axis[0], axis[1], color=c)
-    errors = [a*b/100 for a,b in zip(axis[1],axis[2])]
-    plt.errorbar(axis[0], axis[1], errors, linestyle='None', marker='.', color=c)
+    if (line[1]):
+      c = colors.pop()
+      line[1].sort(key = lambda x: x[0])
+      axis = map(list, zip(*line[1]))
+      subtitle.append(line[0])
+      plt.plot(axis[0], axis[1], color=c)
+      errors = [a*b/100 for a,b in zip(axis[1],axis[2])]
+      plt.errorbar(axis[0], axis[1], errors, linestyle='None', marker='.', color=c)
 
-  plt.legend(subtitle, loc='upper left')
+  box = ax.get_position()
+  ax.set_position([box.x0, box.y0 + box.height * 0.1,
+                 box.width, box.height * 0.9])
+  ax.legend(subtitle, loc='upper center', bbox_to_anchor=(0.5, 1.12), ncol=4)
+  plt.ylabel("time (s)")
+  plt.ylim(ymin=0)
+  plt.xlabel(x_type)
+  plt.xlim(xmin=0)
+
   plt.show()
 
 methods = ["omp", "pth", "seq", "seq_io"]
 regions = ["elephant", "full", "seahorse", "triple_spiral"]
+sizes = [16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
+threads = [1, 2, 4, 8, 16, 32]
 
 data = []
-pattern = "Performance counter stats for '.* (\d+) (\d+)' \(10 runs\):\s+([\d,]+) +task-clock.*\( \+\- +([\d,]+)% \)"
+pattern = "Performance counter stats for '.* ([\d]+) ([\d]+)' \(10 runs\):\s+.*\s+.*\s+.*\s+.*\s+.*\s+.*\s+.*\s+.*\s+.*\s+.*\s+branch-misses\s+([\d\.]+) seconds time elapsed \s+\( \+\-\s+([\d\.]+)\% \)"
+regex = re.compile(pattern)
 
 for method in methods:
   for region in regions:
-    filename = "results/mandelbrot_{0}/{1}.log".format(method, region)
+    filename = "results_final/mandelbrot_{0}/{1}.log".format(method, region)
     textfile = open(filename, 'r')
     filetext = textfile.read()
     textfile.close()
-    matches = re.findall(pattern, filetext)
+    matches = regex.findall(filetext)
     for match in matches:
       item = {}
       item["method"] = method
@@ -60,8 +74,11 @@ for method in methods:
       item["deviation"] = float(match[3].replace(',', '.'))
       data.append(item)
 
-# lines = make_plot({"thread": [1,2,4,8,16,32], "size": 128, "method": ["pth", "omp"], "region": "full", "x": "thread", "line": "method"})
-lines = make_plot({"thread": 1, "size": [16, 32, 64, 128], "method": ["pth", "omp", "seq", "seq_io"], "region": "full", "x": "size", "line": "method"})
+# lines = make_plot({"thread": [1,2,4,8,16,32], "size": 8192, "method": ["pth", "omp"], "region": "full", "x": "thread", "line": "method"})
+# lines = make_plot({"thread": 1, "size": sizes, "method": methods, "region": "full", "x": "size", "line": "method"})
+#lines = make_plot({"thread": 1, "size": sizes, "method": "seq", "region": regions, "x": "size", "line": "region"})
+# lines = make_plot({"thread": threads, "size": 8192, "method": "omp", "region": regions, "x": "thread", "line": "region"})
+lines = make_plot({"thread": threads, "size": 32, "method": methods, "region": "full", "x": "thread", "line": "method"})
 
 # for method in ["omp", "pth"]:
 #   region = "full"
